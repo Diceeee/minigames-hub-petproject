d3cb3e4861680fe2ca6906589748f90e7acf84cc
Dice <yaroslav.shestukhin@gmail.com>
2025-07-01 02:10:04 +0300

    Eureka server app, some cleanup

--- auth/src/main/java/com/dice/auth/core/access/Authorities.java
+++
@@ -1,9 +0,0 @@
-package com.dice.auth.core.access;
-
-public interface Authorities {
-
-    /**
-     * Has access to complete email verification, for new users only.
-     */
-    String VERIFY_EMAIL = "VERIFY_EMAIL";
-}
